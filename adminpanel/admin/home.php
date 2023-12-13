c99a889ec4d03a864625302f661b5f5874f3d37b
Anh Kiet <ct.winak11@gmail.com>
2023-12-14 01:08:11 +0700

    add feedback query

--- adminpanel/admin/home.php
+++ adminpanel/admin/home.php
@@ -1,72 +1,55 @@
-<?php 
+<?php
 session_start();
 
-if(!isset($_SESSION['admin']['adminnakalogin']) == true) header("location:index.php");
+if (!isset($_SESSION['admin']['adminnakalogin']) == true) header("location:index.php");
 
 
- ?>
+?>
 <?php include("../../conn.php"); ?>
 <!-- MAO NI ANG HEADER -->
-<?php include("includes/header.php"); ?>      
+<?php include("includes/header.php"); ?>
 
 <!-- UI THEME DIRI -->
 <!-- <?php include("includes/ui-theme.php"); ?> -->
 
 <div class="app-main">
-<!-- sidebar diri  -->
-<?php include("includes/sidebar.php"); ?>
+  <!-- sidebar diri  -->
+  <?php include("includes/sidebar.php"); ?>
 
 
 
-<!-- Condition If unza nga page gi click -->
-<?php 
-   @$page = $_GET['page'];
+  <!-- Condition If unza nga page gi click -->
+  <?php
+  @$page = $_GET['page'];
 
 
-   if($page != '')
-   {
-     if($page == "add-course")
-     {
-     include("pages/add-course.php");
-     }
-     else if($page == "manage-course")
-     {
-     	include("pages/manage-course.php");
-     }
-     else if($page == "manage-exam")
-     {
+  if ($page != '') {
+    if ($page == "add-course") {
+      include("pages/add-course.php");
+    } else if ($page == "manage-course") {
+      include("pages/manage-course.php");
+    } else if ($page == "manage-exam") {
       include("pages/manage-exam.php");
-     }
-     else if($page == "manage-examinee")
-     {
+    } else if ($page == "manage-examinee") {
       include("pages/manage-examinee.php");
-     }
-     else if($page == "ranking-exam")
-     {
+    } else if ($page == "ranking-exam") {
       include("pages/ranking-exam.php");
-     }
-     else if($page == "feedbacks")
-     {
+    } else if ($page == "feedbacks") {
       include("pages/feedbacks.php");
-     }
-     else if($page == "examinee-result")
-     {
+    } else if ($page == "examinee-result") {
       include("pages/examinee-result.php");
-     }
-
-       
-   }
-   // Else ang home nga page mo display
-   else
-   {
-     include("pages/home.php"); 
-   }
+    }
+  }
+  // Else ang home nga page mo display
+  else {
+    include("pages/home.php");
+  }
 
 
- ?> 
+  ?>
 
 
-<!-- MAO NI IYA FOOTER -->
-<?php include("includes/footer.php"); ?>
+  <!-- MAO NI IYA FOOTER -->
+  <?php include("includes/footer.php"); ?>
 
-<?php include("includes/modals.php"); ?>
+  <?php include("includes/modals.php"); ?>
--- adminpanel/admin/includes/sidebar.php
+++ adminpanel/admin/includes/sidebar.php
@@ -1,127 +1,128 @@
    <div class="app-sidebar sidebar-shadow">
-                    <div class="app-header__logo">
-                        <div class="logo-src"></div>
-                        <div class="header__pane ml-auto">
-                            <div>
-                                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
-                                    <span class="hamburger-box">
-                                        <span class="hamburger-inner"></span>
-                                    </span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="app-header__mobile-menu">
-                        <div>
-                            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
-                                <span class="hamburger-box">
-                                    <span class="hamburger-inner"></span>
-                                </span>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="app-header__menu">
-                        <span>
-                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
-                                <span class="btn-icon-wrapper">
-                                    <i class="fa fa-ellipsis-v fa-w-6"></i>
-                                </span>
-                            </button>
-                        </span>
-                    </div>    <div class="scrollbar-sidebar">
-                        <div class="app-sidebar__inner">
-                            <ul class="vertical-nav-menu">
-                                <li class="app-sidebar__heading"><a href="home.php">Dashboards</a></li>
+       <div class="app-header__logo">
+           <div class="logo-src"></div>
+           <div class="header__pane ml-auto">
+               <div>
+                   <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                       <span class="hamburger-box">
+                           <span class="hamburger-inner"></span>
+                       </span>
+                   </button>
+               </div>
+           </div>
+       </div>
+       <div class="app-header__mobile-menu">
+           <div>
+               <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                   <span class="hamburger-box">
+                       <span class="hamburger-inner"></span>
+                   </span>
+               </button>
+           </div>
+       </div>
+       <div class="app-header__menu">
+           <span>
+               <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                   <span class="btn-icon-wrapper">
+                       <i class="fa fa-ellipsis-v fa-w-6"></i>
+                   </span>
+               </button>
+           </span>
+       </div>
+       <div class="scrollbar-sidebar">
+           <div class="app-sidebar__inner">
+               <ul class="vertical-nav-menu">
+                   <li class="app-sidebar__heading"><a href="home.php">Dashboards</a></li>
 
-                                <li class="app-sidebar__heading">QUẢN LÍ KHÓA HỌC</li>
-                                <li>
-                                    <!-- <a href="#">
+                   <li class="app-sidebar__heading">QUẢN LÍ KHÓA HỌC</li>
+                   <li>
+                       <!-- <a href="#">
                                          <i class="metismenu-icon pe-7s-display2"></i>
                                          Khóa học
                                         <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
                                     </a>
                                     <ul> -->
-                                        <li>
-                                            <a href="#" data-toggle="modal" data-target="#modalForAddCourse">
-                                                <i class="metismenu-icon pe-7s-display2"></i>
-                                                Thêm khóa học
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="home.php?page=manage-course">
-                                            <i class="metismenu-icon pe-7s-display2"></i>
-                                            Quản lí khóa học
-                                            </a>
-                                        </li>
-                                       
-                                    <!-- </ul> -->
-                                </li>
-                               
-                                <li class="app-sidebar__heading">QUẢN LÍ BÀI KIỂM TRA</li>
-                                <li>
-                                    <!-- <a href="#">
+                   <li>
+                       <a href="#" data-toggle="modal" data-target="#modalForAddCourse">
+                           <i class="metismenu-icon pe-7s-display2"></i>
+                           Thêm khóa học
+                       </a>
+                   </li>
+                   <li>
+                       <a href="home.php?page=manage-course">
+                           <i class="metismenu-icon pe-7s-display2"></i>
+                           Quản lí khóa học
+                       </a>
+                   </li>
+
+                   <!-- </ul> -->
+                   </li>
+
+                   <li class="app-sidebar__heading">QUẢN LÍ BÀI KIỂM TRA</li>
+                   <li>
+                       <!-- <a href="#">
                                          <i class="metismenu-icon pe-7s-display2"></i>
                                          Bài kiểm tra
                                         <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
                                     </a>
                                     <ul> -->
-                                        <li>
-                                            <a href="#" data-toggle="modal" data-target="#modalForExam">
-                                                <i class="metismenu-icon pe-7s-display2"></i>
-                                                Thêm bài kiểm tra
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="home.php?page=manage-exam">
-                                            <i class="metismenu-icon pe-7s-display2"></i>
-                                            Quản lí bài kiểm tra
-                                            </a>
-                                        </li>
-                                       
-                                    <!-- </ul> -->
-                                </li>
-                           
-                         
-                                <li class="app-sidebar__heading">QUẢN LÍ HỌC VIÊN</li>
-                                <li>
-                                    <a href="" data-toggle="modal" data-target="#modalForAddExaminee">
-                                        <i class="metismenu-icon pe-7s-add-user">
-                                        </i>Thêm học viên
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="home.php?page=manage-examinee">
-                                        <i class="metismenu-icon pe-7s-users">
-                                        </i>Quản lí học viên
-                                    </a>
-                                </li>
-                                <!-- <li class="app-sidebar__heading">RANKING</li>
-                                <li>
-                                    <a href="home.php?page=ranking-exam">
-                                        <i class="metismenu-icon pe-7s-cup">
-                                        </i>Ranking By Exam
-                                    </a>
-                                </li>
+                   <li>
+                       <a href="#" data-toggle="modal" data-target="#modalForExam">
+                           <i class="metismenu-icon pe-7s-display2"></i>
+                           Thêm bài kiểm tra
+                       </a>
+                   </li>
+                   <li>
+                       <a href="home.php?page=manage-exam">
+                           <i class="metismenu-icon pe-7s-display2"></i>
+                           Quản lí bài kiểm tra
+                       </a>
+                   </li>
+
+                   <!-- </ul> -->
+                   </li>
 
 
-                                <li class="app-sidebar__heading">REPORTS</li>
-                                <li>
-                                    <a href="home.php?page=examinee-result">
-                                        <i class="metismenu-icon pe-7s-cup">
-                                        </i>Examinee Result
-                                    </a>
-                                </li>
-                              
+                   <li class="app-sidebar__heading">QUẢN LÍ HỌC VIÊN</li>
+                   <li>
+                       <a href="" data-toggle="modal" data-target="#modalForAddExaminee">
+                           <i class="metismenu-icon pe-7s-add-user">
+                           </i>Thêm học viên
+                       </a>
+                   </li>
+                   <li>
+                       <a href="home.php?page=manage-examinee">
+                           <i class="metismenu-icon pe-7s-users">
+                           </i>Quản lí học viên
+                       </a>
+                   </li>
+                   <!-- <li class="app-sidebar__heading">RANKING</li>
+                   <li>
+                       <a href="home.php?page=ranking-exam">
+                           <i class="metismenu-icon pe-7s-cup">
+                           </i>Ranking By Exam
+                       </a>
+                   </li> -->
 
-                                 <li class="app-sidebar__heading">FEEDBACKS</li>
-                                <li>
-                                    <a href="home.php?page=feedbacks">
-                                        <i class="metismenu-icon pe-7s-chat">
-                                        </i>All Feedbacks
-                                    </a>
-                                </li> -->
-                                
-                            </ul>
-                        </div>
-                    </div>
-                </div>  
+
+                   <li class="app-sidebar__heading">REPORTS</li>
+                   <li>
+                       <a href="home.php?page=examinee-result">
+                           <i class="metismenu-icon pe-7s-cup">
+                           </i>Examinee Result
+                       </a>
+                   </li>
+
+
+                   <li class="app-sidebar__heading">FEEDBACKS</li>
+                   <li>
+                       <a href="home.php?page=feedbacks">
+                           <i class="metismenu-icon pe-7s-chat">
+                           </i>All Feedbacks
+                       </a>
+                   </li>
+
+               </ul>
+           </div>
+       </div>
+   </div>
